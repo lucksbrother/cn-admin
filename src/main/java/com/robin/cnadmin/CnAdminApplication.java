3d32746a64b61e88272e9ebb42f849624f69b7bf
王斌 <wang_bin@inspurworld.com>
2020-06-11 14:55:07 +0800

    搭建基础项目

---
+++ src/main/java/com/robin/cnadmin/CnAdminApplication.java
@@ -0,0 +1,13 @@
+package com.robin.cnadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robin/cnadmin/CnAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.cnadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
